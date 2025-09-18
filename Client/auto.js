78ea4dfda03229478458ae899d5b1af3842d38d5
ThingWerks <thwerks@protonmail.com>
2025-09-18 19:19:14 +0800

    Create auto.js

---
+++ Client/auto.js
@@ -0,0 +1,53 @@
+#!/usr/bin/node
+hotReload = {};
+module.exports = {
+    ha: {
+        subscribe: [],
+        sync: []
+    },
+    esp: {
+        subscribe: [],
+        heartbeat: []
+    },
+    automation: {
+        example: function (_name, push) {
+            let st, cfg, nv;
+            if (push) {
+                pointers();
+                /*
+
+                    event based logic goes here
+
+                */
+            } else {
+                config[_name] = {};     // initialize automation's configurations
+                state[_name] = {};      // initialize automation's volatile memory
+                pointers();
+                /*
+
+                    initialization logic goes here
+
+                */
+            }
+            /*
+
+                    common functions (initialization and event) go here
+
+            */
+            if (!hotReload[_name]) {
+                log("hot reload initiated");
+                //clear event timers clearInterval(st.timer.second);
+                state[_name] = undefined;
+                config[_name] = undefined;
+            }
+            function pointers() {
+                log = (m, l) => slog(m, l, _name);
+                hotReload[_name] ||= true;
+                nvMem[_name] ||= {};
+                nv = nvMem[_name];
+                cfg = config[_name];
+                st = state[_name];
+            }
+        },
+    }
+}
