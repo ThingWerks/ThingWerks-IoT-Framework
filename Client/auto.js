78757b120d9d4a6ae4a6b992887e5ee98c6510d1
ThingWerks <thwerks@protonmail.com>
2025-10-13 20:29:19 +0800

    Update auto.js

--- Client/auto.js
+++ Client/auto.js
@@ -1,37 +1,24 @@
 #!/usr/bin/node
 module.exports = {
-    ha: {
-        subscribe: [],
-        sync: []
-    },
-    esp: {
-        subscribe: [],
-        heartbeat: []
-    },
-    config: {
-        yourAutomationsName: {
-            entities: [
-                //   "input_boolean.test_switch"
-            ],
-           // configTest: "testing123"
-        }
-    },
+    sync: [],
+    heartbeat: [],
     automation: {
         yourAutomationsName: function (_name, _push, _reload) {
             try {
-                let { st, cfg, nv, log, writeNV } = _pointers(_name);
+                let { state, config, nv, log, write, push } = _pointers(_name);
                 if (_reload) {   // called after modification/reload of this automation file
                     if (_reload != "config") {
                         log("hot reload initiated");
-                        //clear event timers clearInterval(st.timer.second);
-                    } else ({ st, cfg, nv } = _pointers(_name));
+                        //clear event timers clearInterval(state.timer.second);
+                    } else ({ state, config, nv } = _pointers(_name));
                     return;
                 }
                 if (_push === "init") { // ran only once - your initialization procedure
-                    config[_name] = {};     // initialize automation's configurations or from -c config File
-                    state[_name] = {};      // initialize automation's volatile memory
-                    nvMem[_name] ||= {};    // initialize automation's non-volatile memory
-                    ({ st, cfg, nv } = _pointers(_name)); // call pointers directly after config and state initialization 
+                    global.config[_name] = {};  // initialize automation's configurations or from -c config File
+                    global.state[_name] = {};   // initialize automation's volatile memory
+                    global.nv[_name] ||= {};    // initialize automation's non-volatile memory
+                    global.push[_name] = {};    // initialize push functions 
+                    ({ state, config, nv, push } = _pointers(_name)); // call pointers directly after config and state initialization 
 
                     /*
         
@@ -40,19 +27,11 @@ module.exports = {
                     */
 
                     return;
-                } else {    // called with every incoming push event
-
-                    /*
-        
-                        event based logic goes here
-        
-                    */
-
-                }
+                } else st.push[_push.name]?.();     // called with every incoming push event
 
                 /*
         
-                        common functions (initialization and push events) go here
+                        common functions (for initialization and push events) go here
         
                 */
 
@@ -60,31 +39,34 @@ module.exports = {
         },
         example2: function (_name, _push, _reload) {     // add another automation 
             try {
-                let { st, cfg, nv, log, writeNV } = _pointers(_name);
+                let { state, config, nv, log, write, push } = _pointers(_name);
                 if (_reload) {   // called after modification/reload of this automation file
-                    if (_reload != "config") { 
+                    if (_reload != "config") {
                         log("hot reload initiated");
-                    } else ({ st, cfg, nv } = _pointers(_name)); // called after modification/reload of this automations config file
+                    } else ({ state, cfg, nv } = _pointers(_name)); // called after modification/reload of this automations config file
                     return;
                 }
                 if (_push === "init") { // ran only once
-                    config[_name] = {};     // initialize automation's configurations
-                    state[_name] = {};      // initialize automation's volatile memory
-                    nvMem[_name] ||= {};    // initialize automation's non-volatile memory
-                    ({ st, cfg, nv } = _pointers(_name)); // you must call pointers directly after config, state and or NV initialization 
+                    global.config[_name] = {};     // initialize automation's configurations
+                    global.state[_name] = {};      // initialize automation's volatile memory
+                    global.nv[_name] ||= {};    // initialize automation's non-volatile memory
+                    global.push[_name] = {};    // initialize push functions 
+                    ({ state, config, nv, push } = _pointers(_name)); // you must call pointers directly after config, state and or NV initialization 
                     // init sequence here
                     return;
-                } else { }  // event based logic goes here
+                } else st.push[_push.name]?.();
                 /* common functions (for initialization and event shared functions) go here */
             } catch (error) { console.trace(error) }
         },
     },
 }
 let _pointers = (_name) => {
+    push[_name] ||= {}
     return {
-        st: state[_name] ?? undefined,
-        cfg: config[_name] ?? undefined,
-        nv: nvMem[_name] ?? undefined,
+        state: state[_name] ?? undefined,
+        config: config[_name] ?? undefined,
+        nv: nv[_name] ?? undefined,
+        push: push[_name] ?? undefined,
         log: (m, l) => slog(m, l, _name),
         write: () => file.write.nv(_name),
         send: (name, state, unit, address) => { core("state", { name, state, unit, address }, _name) },
