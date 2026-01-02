3584878c941f25f0c44e49a2822f643f8b1b82e9
ThingWerks <thwerks@protonmail.com>
2026-01-02 10:09:02 +0800

    Update auto.js

--- Client/auto.js
+++ Client/auto.js
@@ -4,10 +4,10 @@ module.exports = {
         subscribe: [],
         create: [],
         sync: {},
+        heartbeat: [],
     },
-    heartbeat: [],
     automation: {
-        yourAutomationsName: function (_name, _push, _reload) {
+        "---automation name here----": function (_name, _push, _reload) {
             try {
                 let { state, config, nv, log, save, send, push } = _pointers(_name);
                 if (_reload) {   // called after modification/reload of this automation file
@@ -19,6 +19,14 @@ module.exports = {
                     } else ({ state, config, nv } = _pointers(_name));
                     return;
                 }
+
+                /*
+        
+                        common functions (for initialization and push events) go here
+        
+                */
+
+                        
                 if (_push === "init") { // ran only once - your initialization procedure
                     global.config[_name] = {};  // initialize automation's configurations or from -c config File
                     global.state[_name] = {};   // initialize automation's volatile memory
@@ -36,36 +44,6 @@ module.exports = {
 
                     return;
                 } else push[_push.name]?.(_push.state, _push.name);    // called with every incoming push event
-
-                /*
-        
-                        common functions (for initialization and push events) go here
-        
-                */
-
-            } catch (error) { console.trace(error) }
-        },
-        example2: function (_name, _push, _reload) {     // add another automation 
-            try {
-                let { state, config, nv, log, save, send, push } = _pointers(_name);
-                if (_reload) {   // called after modification/reload of this automation file
-                    if (_reload != "config") {
-                        log("hot reload initiated");
-                        if (push) push.forIn(name => { delete push[name]; })
-                        push = {};  // clean up push initialization 
-                    } else ({ state, config, nv } = _pointers(_name));// called after modification/reload of this automations config file
-                    return;
-                }
-                if (_push === "init") { // ran only once
-                    global.config[_name] = {};     // initialize automation's configurations
-                    global.state[_name] = {};      // initialize automation's volatile memory
-                    global.nv[_name] ||= {};    // initialize automation's non-volatile memory
-                    global.push[_name] = {};    // initialize push functions 
-                    ({ state, config, nv, push } = _pointers(_name)); // you must call pointers directly after config, state and or NV initialization 
-                    // init sequence here
-                    return;
-                } else push[_push.name]?.(_push.state, _push.name);
-                /* common functions (for initialization and event shared functions) go here */
             } catch (error) { console.trace(error) }
         },
     },
