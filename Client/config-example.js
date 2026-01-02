3f21ea6d75eaf6243d489d2d60588a8f4b8a418e
ThingWerks <thwerks@protonmail.com>
2026-01-02 11:46:14 +0800

    Create config-example.js

---
+++ Client/config-example.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entity: {
+        subscribe: [
+            // "input_boolean.test",
+            //  "sunlight",
+            //  "wifi_signal",
+            "test-local-sensor",
+            "lth-relay2",
+            "Lights Outside Bedroom",
+            "input_boolean.test_house",
+            "Button Water Irrigation Piggary",
+        ],
+        create: [
+            "test-local-sensor",
+            //  "test-local-sensor2",
+        ],
+     //   sync: { "testGroup": ["input_boolean.test_switch", "input_boolean.test", "test-led", "test-local-sensor"], },
+    },
+    config: {
+       
+    }
+}
