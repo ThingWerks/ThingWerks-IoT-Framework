9e3a0b1cbff5519698c9407120ac9bec63bfc29f
ThingWerks <thwerks@protonmail.com>
2026-07-01 15:51:55 +0800

    Update twit.js

--- Client/twit.js
+++ Client/twit.js
@@ -1,18 +1,18 @@
 module.exports = {
     framework: (_name) => {
         return {
-            state: state[_name] ?? undefined,
-            config: config[_name] ?? undefined,
-            nv: nv[_name] ?? undefined,
-            push: push[_name] ||= {},
+            state: _state[_name] ?? undefined,
+            config: _config[_name] ?? undefined,
+            nv: _nv[_name] ?? undefined,
+            push: _push[_name] ||= {},
             log: (m, l) => slog(m, l, _name),
             write: () => file.write(_name, "nv"),
             send: (name, s, u, a) => com.core("state", { name, state: s, unit: u, address: a }, _name),
             tool: {
                 debounce: Object.assign((key, delay, callback) => {
                     let registry, gate;
-                    if (_name && global.state[_name]) {
-                        registry = global.state[_name]._debounce ||= {};
+                    if (_name && global._state[_name]) {
+                        registry = global._state[_name]._debounce ||= {};
                         gate = registry[key] ||= { active: false, start: 0 };
 
                         const now = time.epoch;
@@ -32,7 +32,7 @@ module.exports = {
                     }
                 }, {
                     reset: (key) => {
-                        if (global.state[_name]._debounce?.[key]) global.state[_name]._debounce[key].active = false;
+                        if (global._state[_name]._debounce?.[key]) global._state[_name]._debounce[key].active = false;
                     }
                 }),
                 round: function (x, precision) { return Math.round(x * precision) / precision },
